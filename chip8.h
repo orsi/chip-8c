23a8f16787f312e4e13e7893d077c57ddeaffd97
jorsi <jonathon.orsi@gmail.com>
2020-09-03 14:38:47 -0400

    update get_opcode signature

--- chip8.h
+++ chip8.h
@@ -56,7 +56,7 @@ const static uint8_t font_set[] = {
 void load_rom(Chip8 *chip8, const char* file);
 void init(Chip8 *chip8);
 void reset(Chip8 *chip8);
-void get_opcode(Chip8 chip8);
+uint16_t get_opcode(Chip8 *chip8);
 void execute_opcode(Chip8 *chip8);
 void process_user_input(Chip8 *chip8);
 void update_timers(Chip8 *chip8);
